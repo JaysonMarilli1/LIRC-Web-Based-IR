938c9102b8efce26959c11581befdd19b024a036
JaysonMarilli1 <Jayson@mns.local>
2018-03-22 12:54:33 +0200

    LIRC Web Based IR
    Raspberry Pi - Python - Sending TV IR commands from pythons flask web
    pages

---
+++ remote.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+from lirc.lirc import Lirc
+from flask import Flask
+from flask import render_template
+from flask import request, redirect, url_for
+
+BASE_URL = ''
+
+app = Flask(__name__)
+
+# Initialise the Lirc config parser
+lircParse = Lirc('/etc/lirc/lircd.conf')
+
+
+@app.route("/")
+@app.route("/<device>")
+def index(device=None):
+    # Get the devices from the config file
+    devices = []
+    for dev in lircParse.devices():
+        d = {
+            'id': dev,
+            'name': dev,
+        }
+        devices.append(d)
+    
+    return render_template('remote.html', devices=devices)
+
+
+@app.route("/device/<device_id>")
+def device(device_id=None):
+    d = {'id':device_id}        
+    return render_template('control.html', d=d)
+
+
+@app.route("/device/<device_id>/clicked/<op>")
+def clicked(device_id=None, op=None):
+    # Send message to Lirc to control the IR
+   # lircParse.send_once(device_id, op)
+	# if statement to check if volume pressed, if so send 3 commands
+	if op == KEY_VOLUMEUP :
+		lircParse.send_once(device_id, op)
+		lircParse.send_once(device_id, op)
+		lircParse.send_once(device_id, op)
+		return ""
+	elif op == KEY_VOLUMEDOWN :
+		lircParse.send_once(device_id, op)
+		lircParse.send_once(device_id, op)
+		lircParse.send_once(device_id, op)
+		return ""
+	else :
+		lircParse.send_once(device_id, op)
+        return ""
+
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run('0.0.0.0')
+
+
